13e6a51b5806cd50218d6aa489d349bde999a10d
prranesh-arch <81365988+prranesh-arch@users.noreply.github.com>
2022-04-16 16:03:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function startClassification()
+{
+ navigator.mediaDevices.getUserMedia({audio: true});
+ classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/l3AYJQxCN/', modelReady);
+}
+
+function modelReady(){
+ classifier.classify( gotResults);
+}
+
+function gotResuts(error, results) {
+ if (error) {
+    console.error(error);
+ } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+ }
+}
